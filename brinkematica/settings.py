f8a9314174d393d0973b2a9c15721ba2cd1f0f50
Jardel Bastos <84300151+realidademax@users.noreply.github.com>
2023-04-19 08:52:36 -0300

    secondcommit
    Ajustar arquivo settings.py adicionando ALLOWED_HOSTS, STATIC_ROOT e STATICFILES_DIRS.

--- brinkematica/settings.py
+++ brinkematica/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jo07b$83bfida8-0wbc=e_5p1$kph+9&-*vvyi46j#h(b_30lk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
